b8cfcf58826b7c1c6acf54ffd76951d04dea8809
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-02-06 18:11:26 +0100

    Create md5.h

---
+++ src/md5.h
@@ -0,0 +1,39 @@
+/***[head.c]******************************************************[TAB=4]****\
+*                                                                            *
+* GOOMBAServer                                                               *
+*                                                                            *
+* Copyright 2021,2022 GoombaProgrammer & Computa.me                          *
+*                                                                            *
+*  This program is free software; you can redistribute it and/or modify      *
+*  it under the terms of the GNU General Public License as published by      *
+*  the Free Software Foundation; either version 2 of the License, or         *
+*  (at your option) any later version.                                       *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU General Public License for more details.                              *
+*                                                                            *
+*  You should have received a copy of the GNU General Public License         *
+*  along with this program; if not, write to the Free Software               *
+*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+*                                                                            *
+\****************************************************************************/
+#ifndef _md5_h
+#define _md5_h
+
+#include "global.h"
+
+/* MD5 context. */
+typedef struct {
+  UINT4 state[4];                                   /* state (ABCD) */
+  UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
+  unsigned char buffer[64];                         /* input buffer */
+} MD5_CTX;
+
+void MD5Init PROTO_LIST ((MD5_CTX *));
+void MD5Update PROTO_LIST
+  ((MD5_CTX *, unsigned char *, unsigned int));
+void MD5Final PROTO_LIST ((unsigned char [16], MD5_CTX *));
+
+#endif
