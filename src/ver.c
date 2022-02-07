9f7dfcbf0df8ccf10f78180106511bb9e320fa42
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-02-07 12:12:51 +0100

    Create ver.c

---
+++ src/ver.c
@@ -0,0 +1,29 @@
+/***[ver.c]********************************************************[TAB=4]***\
+*                                                                            *
+* GOOMBAServer                                                               *
+*                                                                            *
+* Copyright 2021,2022 GoombaProgrammer & Computa.me                          *
+*                                                                            *
+*  This program is free software; you can redistribute it and/or modify      *
+*  it under the terms of the GNU General Public License as published by      *
+*  the Free Software Foundation; either version 2 of the License, or         *
+*  (at your option) any later version.                                       *
+*                                                                            *
+*  This program is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU General Public License for more details.                              *
+*                                                                            *
+*  You should have received a copy of the GNU General Public License         *
+*  along with this program; if not, write to the Free Software               *
+*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+*                                                                            *
+\****************************************************************************/
+#include <stdlib.h>
+#include <GOOMBAServer.h>
+#include <parse.h>
+#include <stdarg.h>
+
+void goombaver(void){
+ Push(GOOMBAServer_VERSION,STRING); 
+}
